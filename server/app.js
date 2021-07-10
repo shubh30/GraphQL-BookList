9223a7c1be38e82084f0de983c02be9bda10e546
shubh30 <shubhamnagpal30@gmail.com>
2021-07-10 23:20:39 +0530

    Setup MongoDB

--- server/app.js
+++ server/app.js
@@ -1,7 +1,19 @@
+require('dotenv').config()
 const express = require('express')
 const {graphqlHTTP} = require('express-graphql')
 const schema = require('./schema/schema')
+const mongoose = require('mongoose')
 const app = express()
+
+const dbUri = process.env.MONGODB_URI
+
+mongoose.connect(dbUri, { 
+  useNewUrlParser: true
+})
+
+mongoose.connection.once('open', () => {
+  console.log('Connected to DB')
+})
 
 app.use('/graphql', graphqlHTTP({
   schema,
