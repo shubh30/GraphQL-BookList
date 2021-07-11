5e6d2aaf6e9ce6064d595f741de4d6a1eb336d36
shubh30 <shubhamnagpal30@gmail.com>
2021-07-11 21:39:53 +0530

    Add validation in mutation

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,12 +2,14 @@ const graphql = require('graphql')
 const _ = require('lodash')
 const Book = require('../models/book')
 const Author = require('../models/author')
-const { GraphQLObjectType, 
+const { 
+  GraphQLObjectType, 
   GraphQLString, 
   GraphQLSchema, 
   GraphQLID,
   GraphQLInt,
-  GraphQLList
+  GraphQLList,
+  GraphQLNonNull
 } = graphql
 
 const BookType = new GraphQLObjectType({
@@ -78,8 +80,8 @@ const Mutation = new GraphQLObjectType({
     addAuthor: {
       type: AuthorType,
       args: {
-        name: { type: GraphQLString },
-        age: { type: GraphQLInt }
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) }
       },
       resolve(parent, args) {
         let author = new Author({
@@ -92,9 +94,9 @@ const Mutation = new GraphQLObjectType({
     addBook: {
       type: BookType,
       args: {
-        name: { type: GraphQLString },
-        genre: { type: GraphQLString },
-        authorId: { type: GraphQLID }
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        genre: { type: new GraphQLNonNull(GraphQLString) },
+        authorId: { type: new GraphQLNonNull(GraphQLID) }
       },
       resolve(parent, args) {
         let book = new Book({
